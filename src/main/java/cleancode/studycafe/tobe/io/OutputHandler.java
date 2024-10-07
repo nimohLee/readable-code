c6a8e22f59c966f6d74ca63ad5bd00727fe8e52e
nimoh <spakers38@gmail.com>
2024-10-07 22:54:26 +0900

    refactor: 스터디 카페 시작 출력 메시지를 하나의 public 메서드로 통합

--- src/main/java/cleancode/studycafe/tobe/io/OutputHandler.java
+++ src/main/java/cleancode/studycafe/tobe/io/OutputHandler.java
@@ -7,17 +7,23 @@ import java.util.List;
 
 public class OutputHandler {
 
-    public void showWelcomeMessage() {
+    public void showInitiateMessages() {
+        showWelcomeMessage();
+        showAnnouncement();
+        askPassTypeSelection();
+    }
+
+    private void showWelcomeMessage() {
         System.out.println("*** 프리미엄 스터디카페 ***");
     }
 
-    public void showAnnouncement() {
+    private void showAnnouncement() {
         System.out.println("* 사물함은 고정석 선택 시 이용 가능합니다. (추가 결제)");
         System.out.println("* !오픈 이벤트! 2주권 이상 결제 시 10% 할인, 12주권 결제 시 15% 할인! (결제 시 적용)");
         System.out.println();
     }
 
-    public void askPassTypeSelection() {
+    private void askPassTypeSelection() {
         System.out.println("사용하실 이용권을 선택해 주세요.");
         System.out.println("1. 시간 이용권(자유석) | 2. 주단위 이용권(자유석) | 3. 1인 고정석");
     }
