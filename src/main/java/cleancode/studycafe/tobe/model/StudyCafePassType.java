9c5af6027bf53365388a42f6379106c1b7255b8f
nimoh <spakers38@gmail.com>
2024-10-08 01:53:17 +0900

    refactor: 사용자 입력값을 StudyCafePassType 에 추가

--- src/main/java/cleancode/studycafe/tobe/model/StudyCafePassType.java
+++ src/main/java/cleancode/studycafe/tobe/model/StudyCafePassType.java
@@ -1,15 +1,27 @@
 package cleancode.studycafe.tobe.model;
 
+import cleancode.studycafe.tobe.exception.AppException;
+
+import java.util.Arrays;
+
 public enum StudyCafePassType {
 
-    HOURLY("시간 단위 이용권"),
-    WEEKLY("주 단위 이용권"),
-    FIXED("1인 고정석");
+    HOURLY("시간 단위 이용권", "1"),
+    WEEKLY("주 단위 이용권", "2"),
+    FIXED("1인 고정석", "3");
 
     private final String description;
+    private final String userInput;
 
-    StudyCafePassType(String description) {
+    StudyCafePassType(String description, String userInput) {
         this.description = description;
+        this.userInput = userInput;
     }
 
+    public static StudyCafePassType findByUserInput(String userInput) {
+        return Arrays.stream(values())
+            .filter(type -> userInput.equals(type.userInput))
+            .findFirst()
+            .orElseThrow(() -> new AppException("잘못된 입력입니다."));
+    }
 }
