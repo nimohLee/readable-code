44f55993a91ecb9f71f1e7cb316156ae9e459008
nimoh <spakers38@gmail.com>
2024-10-09 00:18:31 +0900

    refactor: Locker를 구매할 수 있는 정책 인터페이스 추가

--- src/main/java/cleancode/studycafe/tobe/StudyCafeApplication.java
+++ src/main/java/cleancode/studycafe/tobe/StudyCafeApplication.java
@@ -1,13 +1,16 @@
 package cleancode.studycafe.tobe;
 
 import cleancode.studycafe.tobe.io.StudyCafeFileHandler;
+import cleancode.studycafe.tobe.policy.LockerPolicy;
+import cleancode.studycafe.tobe.policy.LockerFixedPassPolicy;
 
 public class StudyCafeApplication {
 
     public static void main(String[] args) {
         StudyCafeFileHandler studyCafeFileHandler = new StudyCafeFileHandler();
 
-        StudyCafePassMachine studyCafePassMachine = new StudyCafePassMachine(studyCafeFileHandler.readStudyCafePasses(), studyCafeFileHandler.readLockerPasses());
+        LockerPolicy lockerPolicy = new LockerFixedPassPolicy();
+        StudyCafePassMachine studyCafePassMachine = new StudyCafePassMachine(studyCafeFileHandler.readStudyCafePasses(), studyCafeFileHandler.readLockerPasses(), lockerPolicy);
         studyCafePassMachine.run();
     }
 
